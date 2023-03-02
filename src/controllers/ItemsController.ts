dd6779bb93346c16066081ef558e9b333e79ab2c
Fearinn <matheusmgomes09@gmail.com>
2023-03-02 18:05:13 -0300

    feat: pagination added to all routes

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -1,27 +1,72 @@
 import instance from "../services/index.js";
 import express from "express";
+import pagination from "../utils/pagination.js";
+import convertAndCeil from "../utils/convertAndCeil.js";
 
 class AvatarItemsController {
-  static getAll = async (_: express.Request, response: express.Response) => {
-    const { data, status } = await instance.get("/items/avatarItems");
-    console.log(data);
-    response.status(status).json(data);
+  static getAll = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const { data, status } = await instance.get(
+      "/items/avatarItems"
+    );
+
+    if (!Array.isArray(data)) {
+      throw new Error("Type of response data don't match the expected type");
+    }
+    const limit = convertAndCeil(request.query.limit) || 20;
+    const page = convertAndCeil(request.query.page) || 1;
+    const numberOfPages = convertAndCeil(data.length / limit);
+    const dataPage = pagination(data, page, limit);
+
+    response.status(status).json({
+      itemsCount: dataPage.length,
+      totalItemsCount: data.length,
+      pagesCount: numberOfPages,
+      currentPage: page,
+      data: dataPage,
+    });
   };
 }
 
 class AvatarItemsSetsController {
-  static getAll = async (_: express.Request, response: express.Response) => {
+  static getAll = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
     const { data, status } = await instance.get("/items/avatarItemSets");
-    console.log(data);
-    response.status(status).json(data);
+    const limit = convertAndCeil(request.query.limit) || 20;
+    const page = convertAndCeil(request.query.page) || 1;
+    const numberOfPages = convertAndCeil(data.length / limit);
+    const dataPage = pagination(data, page, limit);
+
+    response.status(status).json({
+      totalItemsCount: data.length,
+      pagesCount: numberOfPages,
+      currentPage: page,
+      data: dataPage,
+    });
   };
 }
 
 class RoleIconsController {
-  static getAll = async (_: express.Request, response: express.Response) => {
+  static getAll = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
     const { data, status } = await instance.get("/items/roleIcons");
-    console.log(data);
-    response.status(status).json(data);
+    const limit = convertAndCeil(request.query.limit) || 20;
+    const page = convertAndCeil(request.query.page) || 1;
+    const numberOfPages = convertAndCeil(data.length / limit);
+    const dataPage = pagination(data, page, limit);
+
+    response.status(status).json({
+      totalItemsCount: data.length,
+      pagesCount: numberOfPages,
+      currentPage: page,
+      data: dataPage,
+    });
   };
 }
 
