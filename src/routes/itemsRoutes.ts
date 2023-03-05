d36b516d8ab794c0cd3f8367a93d5901d047635f
Fearinn <matheusmgomes09@gmail.com>
2023-03-05 15:04:42 -0300

    refactor: untyped routes temporarily disabled

--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import {
   AvatarItemsController,
-  AvatarItemsSetsController,
-  RoleIconsController,
+  // AvatarItemsSetsController,
+  // RoleIconsController,
 } from "../controllers/ItemsController.js";
 
 const router = express.Router();
 
 router
   .get("/items/avatarItems", AvatarItemsController.getAll)
-  .get("/items/avatarItemsSets", AvatarItemsSetsController.getAll)
-  .get("/items/roleIcons", RoleIconsController.getAll);
+  // .get("/items/avatarItemsSets", AvatarItemsSetsController.getAll)
+  // .get("/items/roleIcons", RoleIconsController.getAll);
 
 export default router;
