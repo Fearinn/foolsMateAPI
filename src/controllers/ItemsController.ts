e6e61fbe77628a1eb92e52a4beae96896c903764
Fearinn <matheusmgomes09@gmail.com>
2023-03-05 15:07:24 -0300

    refactor: AvatarItems controller made type safer

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -1,6 +1,9 @@
 import instance from "../services/index.js";
 import express from "express";
 import pagination from "../utils/pagination.js";
+import { convertIntoAvatarItemsList } from "../utils/isAvatarItems.js";
+import avatarItemsFilter from "../utils/avatarItemsFilter.js";
+import IAvatarItem from "../types/AvatarItem.js";
 
 class AvatarItemsController {
   static getAll = async (
@@ -9,12 +12,19 @@ class AvatarItemsController {
   ) => {
     try {
       const { data, status } = await instance.get("/items/avatarItems");
+
       if (!Array.isArray(data)) {
         throw new Error("Type of response data don't match the expected type");
       }
+
       const limit = request.query.limit;
       const page = request.query.page;
-      const dataPage = pagination(data, page, limit);
+      // const reqBody: IAvatarItem = request.body
+
+      const safeData = convertIntoAvatarItemsList(data);
+      // const filteredData = avatarItemsFilter(safeData, reqBody);
+
+      const dataPage = pagination<IAvatarItem>(safeData, safeData, page, limit);
 
       response.status(status).json(dataPage);
     } catch (error) {
@@ -26,56 +36,58 @@ class AvatarItemsController {
   };
 }
 
-class AvatarItemsSetsController {
-  static getAll = async (
-    request: express.Request,
-    response: express.Response
-  ) => {
-    try {
-      const { data, status } = await instance.get("/items/avatarItemSets");
-      if (!Array.isArray(data)) {
-        throw new Error("Type of response data don't match the expected type");
-      }
-      const limit = request.query.limit;
-      const page = request.query.page;
-      const dataPage = pagination(data, page, limit);
+// class AvatarItemsSetsController {
+//   static getAll = async (
+//     request: express.Request,
+//     response: express.Response
+//   ) => {
+//     try {
+//       const { data, status } = await instance.get("/items/avatarItemSets");
+//       if (!Array.isArray(data)) {
+//         throw new Error("Type of response data don't match the expected type");
+//       }
 
-      response.status(status).json(dataPage);
-    } catch (error) {
-      console.log(error);
-      response
-        .status(501)
-        .send("An unexpected error occurred! Please try again later");
-    }
-  };
-}
+//       const limit = request.query.limit;
+//       const page = request.query.page;
+//       const dataPage = pagination(data, page, limit);
 
-class RoleIconsController {
-  static getAll = async (
-    request: express.Request,
-    response: express.Response
-  ) => {
-    try {
-      const { data, status } = await instance.get("/items/roleIcons");
-      if (!Array.isArray(data)) {
-        throw new Error("Type of response data don't match the expected type");
-      }
-      const limit = request.query.limit;
-      const page = request.query.page;
-      const dataPage = pagination(data, page, limit);
+//       response.status(status).json(dataPage);
+//     } catch (error) {
+//       console.log(error);
+//       response
+//         .status(501)
+//         .send("An unexpected error occurred! Please try again later");
+//     }
+//   };
+// }
 
-      response.status(status).json(dataPage);
-    } catch (error) {
-      console.log(error);
-      response
-        .status(501)
-        .send("An unexpected error occurred! Please try again later");
-    }
-  };
-}
+// class RoleIconsController {
+//   static getAll = async (
+//     request: express.Request,
+//     response: express.Response
+//   ) => {
+//     try {
+//       const { data, status } = await instance.get("/items/roleIcons");
+//       if (!Array.isArray(data)) {
+//         throw new Error("Type of response data don't match the expected type");
+//       }
+
+//       const limit = request.query.limit;
+//       const page = request.query.page;
+//       const dataPage = pagination(data, page, limit);
+
+//       response.status(status).json(dataPage);
+//     } catch (error) {
+//       console.log(error);
+//       response
+//         .status(501)
+//         .send("An unexpected error occurred! Please try again later");
+//     }
+//   };
+// }
 
 export {
   AvatarItemsController,
-  AvatarItemsSetsController,
-  RoleIconsController,
+  // AvatarItemsSetsController,
+  // RoleIconsController,
 };
