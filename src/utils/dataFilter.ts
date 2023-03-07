9c6e7fc2c20db7796d234f0f5f539aa477c9db1e
Fearinn <matheusmgomes09@gmail.com>
2023-03-07 18:58:46 -0300

    fix: gender filter properly handled

--- src/utils/dataFilter.ts
+++ src/utils/dataFilter.ts
@@ -2,9 +2,9 @@ function dataFilter<T = unknown>(list: T[], body: Partial<T>) {
   let filteredList = list;
   for (const prop in body) {
     filteredList = filteredList.filter((item) => {
+      if (item[prop] === body[prop]) return true;
       if (body[prop] === "" && !item[prop]) return true;
-      if (!body[prop]) return true;
-      return item[prop] === body[prop];
+      if (!body[prop] && body[prop] !== "") return true;
     });
   }
   return filteredList;
