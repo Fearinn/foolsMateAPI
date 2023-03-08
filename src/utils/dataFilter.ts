c2b70c7a9dac128f54496dd35e83ab5587f50927
Fearinn <matheusmgomes09@gmail.com>
2023-03-08 19:31:32 -0300

    feat: filters with RegExp made possible

--- src/utils/dataFilter.ts
+++ src/utils/dataFilter.ts
@@ -2,6 +2,12 @@ function dataFilter<T = unknown>(list: T[], body: Partial<T>) {
   let filteredList = list;
   for (const prop in body) {
     filteredList = filteredList.filter((item) => {
+      if (
+        body[prop] instanceof RegExp &&
+        typeof item[prop] === "string" &&
+        (body[prop] as RegExp).test(item[prop] as string)
+      )
+        return true;
       if (item[prop] === body[prop]) return true;
       if (body[prop] === "" && !item[prop]) return true;
       if (!body[prop] && body[prop] !== "") return true;
