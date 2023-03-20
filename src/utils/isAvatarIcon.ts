4416378b3e91fd9587f82eefd1d20ff7a5c7832b
Fearinn <matheusmgomes09@gmail.com>
2023-03-20 16:59:36 -0300

    fix(isAvatarIcon): import path updated

--- src/utils/isAvatarIcon.ts
+++ src/utils/isAvatarIcon.ts
@@ -1,4 +1,4 @@
-import IRoleIcon from "../types/RoleIcons";
+import IRoleIcon from "../types/RoleIcon";
 
 function isAvatarIcon(item: unknown): item is IRoleIcon {
     return !!(item as IRoleIcon).rarity && !!(item as IRoleIcon).roleId;
