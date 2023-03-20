82cde6405e9bb5535c337571cdc43e0918d59dd2
Fearinn <matheusmgomes09@gmail.com>
2023-03-20 16:37:35 -0300

    feat: roleIcons route added

--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-  AvatarItemsController,
+  AvatarItemsController, RoleIconsController,
 } from "../controllers/ItemsController.js";
 
 const router = express.Router();
 
 router
   .get("/items/avatarItems", AvatarItemsController.getAll)
+  .get("/items/roleIcons", RoleIconsController.getAll)
 
 export default router;
