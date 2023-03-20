20724866aa6800c0a9815dbaac888cefe36404cd
Fearinn <matheusmgomes09@gmail.com>
2023-03-20 17:28:36 -0300

    refactor: cors limited to front-end domain

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import routes from "./routes/index.js";
-import cors from "cors"
-import * as dotenv from "dotenv"
+import cors from "cors";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://wolvesville-wiki.vercel.app",
+    optionsSuccessStatus: 200,
+  })
+);
 
 const port = process.env.PORT || 3000;
 
