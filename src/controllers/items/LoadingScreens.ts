d087c491aaaf1e00e96c497479efcc6265a6e315
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-27 16:53:54 -0300

    refactor: LoadingScreen controller created

---
+++ src/controllers/items/LoadingScreens.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import instance from "../../services/index.js";
+import { filterByType } from "../../utils/filterByType.js";
+import { isLoadingScreen } from "../../utils/isLoadingScreen.js";
+
+export class LoadingScreensController {
+  static getRandom = async (_: express.Request, response: express.Response) => {
+    try {
+      const { data, status } = await instance.get("/items/loadingScreens");
+
+      if (!Array.isArray(data)) {
+        throw new Error("Type of response data don't match the expected type");
+      }
+
+      const filteredData = filterByType(data, isLoadingScreen);
+
+      const randomIndex = Math.ceil(Math.random() * data.length);
+      const randomLoadingScreen = filteredData[randomIndex];
+
+      response.status(status).json(randomLoadingScreen);
+    } catch (error) {
+      console.log(error);
+      response
+        .status(500)
+        .send("An unexpected error occurred! Please try again later");
+    }
+  };
+}
