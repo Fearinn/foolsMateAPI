6214e138c507f7659f0bdb437968c60582670921
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-28 19:12:22 -0300

    refactor(filterByType): additional condition added to prevent errors

--- src/utils/filterByType.ts
+++ src/utils/filterByType.ts
@@ -1,6 +1,8 @@
-export function filterByType<T>(list: unknown[], typeGuard: (item: unknown) => boolean): T[] {
-    const avatarItemsList = list.filter((item): item is T =>
-      typeGuard(item)
-    );
-    return avatarItemsList;
-  }
+export function filterByType<T>(
+  list: unknown[],
+  typeGuard: (item: unknown) => boolean
+): T[] {
+  if (!list.length) return [];
+  const avatarItemsList = list.filter((item): item is T => typeGuard(item));
+  return avatarItemsList;
+}
