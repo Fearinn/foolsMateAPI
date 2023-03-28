37340d689ceb7c547332f5160ef7fe41d3121a24
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-28 20:41:27 -0300

    refactor(AvatarItemsController): getByIds method created

--- src/controllers/items/AvatarItems.ts
+++ src/controllers/items/AvatarItems.ts
@@ -8,7 +8,7 @@ import { paginate } from "../../utils/pagination.js";
 import {
   isAvatarItem,
   isAvatarItemGender,
-  isAvatarItemType
+  isAvatarItemType,
 } from "../../utils/typeGuards/isAvatarItems.js";
 
 export class AvatarItemsController {
@@ -57,6 +57,44 @@ export class AvatarItemsController {
       response.status(status).json(dataPage);
     } catch (error) {
       console.log(error);
+      response
+        .status(500)
+        .send("An unexpected error occurred! Please try again later");
+    }
+  };
+
+  static getByIds = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    try {
+      const axiosResponse = await instance.get("/items/avatarItems");
+
+      if (!axiosResponse || !Array.isArray(axiosResponse.data)) {
+        throw new Error("Type of response data don't match the expected type");
+      }
+
+      const { data, status } = axiosResponse;
+
+      const ids = request.body.ids;
+
+      const safeIds = filterByType<string>(ids, (id) => typeof id === "string");
+
+      const safeData = filterByType<IAvatarItem>(data, isAvatarItem);
+
+      const selectedItems = safeData.filter((item) =>
+        safeIds.includes(item.id)
+      );
+
+      if (selectedItems.length <= 0) {
+        response
+          .status(404)
+          .send("No item was found with the given parameters");
+        return;
+      }
+
+      response.status(status).send(selectedItems);
+    } catch (error) {
       response
         .status(500)
         .send("An unexpected error occurred! Please try again later");
