8e5ea9a22a165c965443d48d998ee969d9e698fe
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-29 14:40:10 -0300

    refactor(typeGuards): add isRewardType

--- src/types/Season.ts
+++ src/types/Season.ts
@@ -1,4 +1,4 @@
-export type Season = {
+export type ISeason = {
   startTime: string;
   number: number;
   durationInDays: number;
@@ -6,13 +6,25 @@ export type Season = {
   goldPricePerReward: number;
   gemPricePerReward: number;
   xpPerReward: number;
-  rewards: IRewards[];
+  rewards: IReward[];
   iconUrl: string;
   seasonBackgroundId: string;
 };
 
-type IRewards = {
-  type: string;
+type IReward = {
+  type: IRewardType;
   amount: number;
   free: boolean;
+  avatarItemIdMale?: string;
+  avatarItemIdFemale?: string;
 };
+
+export type IRewardType =
+  | "AVATAR_ITEM"
+  | "ROLE_ICON"
+  | "ROSE_PACKAGE"
+  | "GOLD"
+  | "GEM"
+  | "EMOJI"
+  | "ROLE_CARD_ABILITY_EXCHANGE_VOUCHER"
+  | "LOADING_SCREEN";
--- src/utils/typeGuards/isSeason.ts
+++ src/utils/typeGuards/isSeason.ts
@@ -1,9 +1,23 @@
-import { Season } from "../../types/Season";
+import { IRewardType, ISeason } from "../../types/Season";
 
-export function isSeason(item: unknown): item is Season {
+export function isSeason(item: unknown): item is ISeason {
   return (
-    !!(item as Season).durationInDays &&
-    !!(item as Season).seasonBackgroundId &&
-    !!(item as Season).rewards
+    !!(item as ISeason).durationInDays &&
+    !!(item as ISeason).seasonBackgroundId &&
+    !!(item as ISeason).rewards
+  );
+}
+
+export function isRewardType(item: unknown): item is IRewardType {
+  if (typeof item !== "string") return false;
+  return (
+    item === "AVATAR_ITEM" ||
+    item === "ROLE_ICON" ||
+    item === "ROSE_PACKAGE" ||
+    item === "GOLD" ||
+    item === "GEM" ||
+    item === "EMOJI" ||
+    item === "ROLE_CARD_ABILITY_EXCHANGE_VOUCHER" ||
+    item === "LOADING_SCREEN"
   );
 }
