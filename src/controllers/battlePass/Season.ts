0297873c83ddb2efa3516a116d853df862c6b4ad
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-15 10:12:27 -0300

    refactor(SeasonController)!: get rewardsTypes from req.query and apply error middleware

--- src/controllers/battlePass/Season.ts
+++ src/controllers/battlePass/Season.ts
@@ -3,39 +3,54 @@ import { instance } from "../../services/index.js";
 import { IRewardType, ISeason } from "../../types/Season.js";
 import { filterByType } from "../../utils/filterByType.js";
 import { isRewardType, isSeason } from "../../utils/typeGuards/isSeason.js";
+import { BaseError } from "../../utils/errors/BaseError.js";
+import { BadRequest } from "../../utils/errors/BadRequest.js";
 
 export class SeasonController {
-  static getAll = async (_: express.Request, response: express.Response) => {
-    try {
-      const { data, status } = await instance.get("/battlePass/season");
-
-      if (!isSeason(data)) {
-        throw new Error("Type of response data doesn't match the expected type");
-      }
-
-      response.status(status).json(data);
-    } catch (error) {
-      console.log(error);
-      response
-        .status(500)
-        .send("An unexpected error occurred! Please try again later");
-    }
-  };
-
-  static getByRewardsType = async (
-    request: express.Request,
-    response: express.Response
+  static getAll = async (
+    _: express.Request,
+    response: express.Response,
+    next: express.NextFunction
   ) => {
     try {
       const { data, status } = await instance.get("/battlePass/season");
 
       if (!isSeason(data)) {
-        throw new Error("Type of response data doesn't match the expected type");
+        throw new BaseError(
+          "Type of response data doesn't match the expected type"
+        );
       }
 
-      const rewardsTypes = request.body.types;
+      response.status(status).json(data);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  static getByRewardsType = async (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const { data, status } = await instance.get("/battlePass/season");
+
+      const { rewardsTypes = "" } = request.query;
+
+      if (typeof rewardsTypes !== "string") {
+        throw new BadRequest();
+      }
+
+      if (!isSeason(data)) {
+        throw new BaseError(
+          "Type of response data doesn't match the expected type"
+        );
+      }
+
+      const typesList = rewardsTypes.split(":");
+
       const safeRewardsTypes = filterByType<IRewardType>(
-        rewardsTypes,
+        typesList,
         isRewardType
       );
 
@@ -49,11 +64,8 @@ export class SeasonController {
       };
 
       response.status(status).json(filteredData);
-    } catch (error) {
-      console.log(error);
-      response
-        .status(500)
-        .send("An unexpected error occurred! Please try again later");
+    } catch (err) {
+      next(err);
     }
   };
 }
