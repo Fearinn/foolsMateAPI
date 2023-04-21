fb7d66bb19ac201acd4d902f609b7880b7342809
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-21 13:55:27 -0300

    refactor(AvatarItemsController): rework getAll and create updateAll

--- src/controllers/items/AvatarItems.ts
+++ src/controllers/items/AvatarItems.ts
@@ -1,55 +1,59 @@
 import express from "express";
+import mongoose from "mongoose";
 import { z } from "zod";
+import { AvatarItemModel } from "../../models/AvatarItems.js";
 import { instance } from "../../services/index.js";
+import { TAggregateRequest } from "../../types/Aggregate.js";
 import {
   ZAvatarItem,
   ZAvatarItemGender,
   ZAvatarItemType,
 } from "../../types/AvatarItem.js";
 import { TDataRequest } from "../../types/DataRequest.js";
-import { ZRarity } from "../../types/Rarity.js";
-import { dataFilter } from "../../utils/dataFilter.js";
 import { ZId } from "../../types/Id.js";
+import { ZRarity } from "../../types/Rarity.js";
 
 const partialItem = ZAvatarItem.partial();
 
 export class AvatarItemsController {
   static getAll = async (
-    request: TDataRequest<typeof partialItem>,
+    request: TAggregateRequest<z.infer<typeof partialItem>>,
     _: express.Response,
     next: express.NextFunction
   ) => {
     try {
-      const { data } = await instance.get("/items/avatarItems");
-
-      const parsedData = ZAvatarItem.array().parse(data);
-
-      const { gender, costInGold, costInRoses, rarity, type, event } =
-        request.query;
+      const {
+        gender = null,
+        rarity = null,
+        type = null,
+        event = null,
+      } = request.query;
 
       const parsedEvent = z
         .string()
-        .optional()
+        .nullable()
         .parse(event)
-        ?.replace(/\s+/ig, "_");
+        ?.replace(/\s+/gi, "_");
 
-      console.log(parsedEvent);
+      const filterBody: mongoose.FilterQuery<z.infer<typeof partialItem>> = {};
 
-      const filterBody = {
-        gender: ZAvatarItemGender.optional().parse(gender),
-        costInGold: z.number().optional().parse(costInGold),
-        costInRoses: z.number().optional().parse(costInRoses),
-        type: ZAvatarItemType.optional().parse(type),
-        rarity: ZRarity.optional().parse(rarity),
-        event: parsedEvent ? new RegExp(parsedEvent, "i") : parsedEvent,
-      };
+      if (gender) filterBody.gender = ZAvatarItemGender.parse(gender);
+      if (rarity) filterBody.rarity = ZRarity.parse(rarity);
+      if (type) filterBody.type = ZAvatarItemType.parse(type);
+      if (parsedEvent)
+        filterBody.event = { $regex: parsedEvent, $options: "im" };
 
-      const filteredData = dataFilter<Partial<z.infer<typeof ZAvatarItem>>>(
-        parsedData,
-        filterBody
-      );
+      const data = AvatarItemModel.aggregate<z.infer<typeof ZAvatarItem>>([
+        {
+          $match: filterBody,
+        },
+      ]);
 
-      request.data = filteredData;
+      const parsedData = z
+        .instanceof(mongoose.Aggregate<z.infer<typeof partialItem>>)
+        .parse(data);
+
+      request.data = parsedData;
 
       next();
     } catch (err) {
@@ -79,6 +83,39 @@ export class AvatarItemsController {
 
       request.data = selectedItems;
       next();
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  static updateAll = async (
+    _: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const { data } = await instance.get("/items/avatarItems");
+
+      const parsedData = ZAvatarItem.array().parse(data);
+
+      const dataInsertion = parsedData.map((item) => {
+        return {
+          updateOne: {
+            filter: { id: item.id },
+            upsert: true,
+            document: item,
+            update: {
+              $set: item,
+            },
+          },
+        };
+      });
+
+      const result = await AvatarItemModel.bulkWrite(dataInsertion, {
+        ordered: false,
+      });
+
+      response.status(200).send(result);
     } catch (err) {
       next(err);
     }
