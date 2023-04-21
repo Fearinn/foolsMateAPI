99a9295e1da5c22ec7d42f9b70f793685961cf4b
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-21 13:51:59 -0300

    refactor(middlewares + utils): create 'handleDbPagination'

---
+++ src/middlewares/handleDbPagination.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import { z } from "zod";
+import { TAggregateResponse, TAggregateRequest } from "../types/Aggregate.js";
+import { paginateDb } from "../utils/paginateDb.js";
+
+export async function handleDbPagination(
+  req: TAggregateRequest<TAggregateResponse>,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  try {
+    const data = req.data;
+
+    const aggregateValidator = z.instanceof(
+      mongoose.Aggregate<TAggregateResponse>
+    );
+
+    if (!aggregateValidator.safeParse(data).success) {
+      next();
+    }
+
+    const parsedData = aggregateValidator.parse(data);
+
+    const { page = 1, limit = 10 } = req.query;
+
+    const parsedPage = z.coerce.number().parse(page);
+    const parsedLimit = z.coerce.number().parse(limit);
+
+    const paginatedData = await paginateDb(parsedData, parsedPage, parsedLimit);
+
+    res.status(200).send(paginatedData);
+  } catch (err) {
+    next(err);
+  }
+}
---
+++ src/utils/paginateDb.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { TAggregateResponse } from "../types/Aggregate";
+
+export async function paginateDb(
+  data: mongoose.Aggregate<TAggregateResponse>,
+  page = 1,
+  limit = 10
+) {
+  const dbPagination = await data
+    .facet({
+      totalData: [{ $skip: limit * (page - 1) }, { $limit: limit }],
+      totalCount: [{ $count: "count" }],
+    })
+    .exec();
+
+    console.log(dbPagination)
+
+  const totalData = dbPagination[0].totalData;
+  const totalCount =
+    totalData.length > 0 ? dbPagination[0].totalCount[0].count : 0;
+  const numberOfPages = Math.ceil(totalCount / limit);
+  const realPage = page > numberOfPages ? numberOfPages : page;
+
+  const paginatedData = {
+    count: totalData.length,
+    totalCount,
+    numberOfPages,
+    currentPage: realPage,
+    items: totalData,
+  };
+
+  return paginatedData;
+}
