2c3b1c74d873f38ca712a6e8183f72151bfd2784
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-28 16:59:40 -0300

    fix(handleSimplePagination): call next if req.data is not array

--- src/middlewares/handleSimplePagination.ts
+++ src/middlewares/handleSimplePagination.ts
@@ -10,14 +10,25 @@ export async function handleSimplePagination<T extends z.ZodTypeAny>(
 ) {
   try {
     const data = req.data;
-    const parsedArray = z.unknown().array().parse(data);
+
+    const arrayValidator = z.unknown().array();
+
+    if (!arrayValidator.safeParse(data).success) {
+      next();
+    }
+
+    const parsedArray = arrayValidator.parse(data);
 
     const { page = 1, limit = 10 } = req.query;
 
     const parsedPage = z.coerce.number().parse(page);
     const parsedLimit = z.coerce.number().parse(limit);
 
-    const paginatedAsArray = paginateSimply(parsedArray, parsedPage, parsedLimit);
+    const paginatedAsArray = paginateSimply(
+      parsedArray,
+      parsedPage,
+      parsedLimit
+    );
 
     res.status(200).send(paginatedAsArray);
   } catch (err) {
